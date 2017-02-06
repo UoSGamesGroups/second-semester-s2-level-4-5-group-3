b115e8010d5931f55826f79c79d4d9d3f6a30070
Ryan Jake Manthorp <ryanmanthorp@hotmail.co.uk>
2017-02-06 20:03:45 +0000

    Added basic working downhill prototype

---
+++ Prototypes/Downhill/Assets/Scripts/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public float jumpSpeed = 10f;
+    public bool Is_Grounded;
+    private Player player;
+
+    void Start()
+    {
+        player = gameObject.GetComponentInParent<Player>();
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        player.Is_Grounded = true;
+    }
+
+    void OnCollisionExit2D(Collision2D coll)
+    {
+        player.Is_Grounded = false;
+    }
+
+    void FixedUpdate ()
+    {
+        if (Input.GetKey(KeyCode.Space)&&Is_Grounded==true)
+            GetComponent<Rigidbody2D>().AddForce(Vector2.up * jumpSpeed, ForceMode2D.Force);
+
+    }
+}
