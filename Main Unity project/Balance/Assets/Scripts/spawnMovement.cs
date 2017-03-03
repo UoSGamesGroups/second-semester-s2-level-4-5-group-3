26d93b808dc6ade42b9e5e97441d2d6adce0ce22
Ryan Jake Manthorp <ryanmanthorp@hotmail.co.uk>
2017-03-03 12:59:43 +0000

    Added random position for item spawner

---
+++ Main Unity project/Balance/Assets/Scripts/spawnMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnMovement : MonoBehaviour
+{
+
+    void Start()
+    {
+        InvokeRepeating("SetRandomPos", 0, 5);
+    }
+
+    void SetRandomPos()
+    {
+        Vector3 temp = new Vector3(Random.Range(-2.2f, 1.0f), Random.Range(3.0f, 2.0f), Random.Range(-0f, 0f));
+        transform.position = temp;
+    }
+}
