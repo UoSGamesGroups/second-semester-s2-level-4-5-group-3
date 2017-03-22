3a4450492882e34ca8297e40290a3d0e44518aef
RazvanM23 <razvan.muresan23@gmail.com>
2017-03-22 07:26:02 +0000

    Added comprehensive menu

---
+++ Main Unity project/Balance/Assets/Scripts/CharacterSelection.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CharacterSelection : MonoBehaviour {
+
+	private List<GameObject> characters;
+	private int selectionIndex = 0; 
+
+
+	private void Start()
+
+	{
+		characters = new List<GameObject> ();
+		foreach (Transform t in transform) 
+		{
+			characters.Add (t.gameObject);
+			t.gameObject.SetActive (false);
+		}
+
+		characters [selectionIndex].SetActive (true);
+	}
+
+	private void Update()
+	{
+		
+	}
+
+	public void Select(int index)
+	{
+		if (index == selectionIndex)
+			return; 
+		if (index < 0 || index >= characters.Count)
+			return;
+		characters [selectionIndex].SetActive (false);
+		selectionIndex = index;
+		characters [selectionIndex].SetActive (true);
+	}
+
+	public void ConfirmButton()
+	{
+		SceneManager.LoadScene ("SelectTimer");
+	}
+}
---
+++ Main Unity project/Balance/Assets/Scripts/TimerSelect.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TimerSelect : MonoBehaviour {
+
+	public void Start30(string Start30)
+	{
+		SceneManager.LoadScene (Start30);
+	}
+
+	public void Start60(string Start60)
+	{
+		SceneManager.LoadScene (Start60);
+	}
+
+	public void Start120(string Start120)
+	{
+		SceneManager.LoadScene (Start120);
+	}
+
+}
